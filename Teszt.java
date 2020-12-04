2082984b5b91a852d285ccce4a3e83297733d679
LengyHELL <ubi19998@gmail.com>
2020-12-04 02:02:19 +0100

    Trying out new functions

--- Teszt.java
+++ Teszt.java
@@ -2,27 +2,40 @@ import java.util.List;
 
 class Teszt {
   public static void main(String args[]) {
-    Bolt bolt = new Bolt("asd", "fgh");
-    Termek sajt = new Etel("Trappista", 1100, Reszleg.TEJ, Iz.SOS);
+    try {
+      Bolt bolt = new Bolt("asd", "fgh");
+      Termek sajt = new Etel("Trappista", 1100, Reszleg.TEJ, Iz.SOS);
 
-    bolt.hozzaad(new Termek("Tej", 300, Reszleg.TEJ));
-    bolt.hozzaad(new Etel("Oreo", 520, Reszleg.KEKSZ, Iz.EDES));
-    bolt.hozzaad(sajt);
-    bolt.hozzaad(new TartosCikk("Monitor", 50000, Reszleg.ELEKTRONIKAI, "Samsung", 2));
-    bolt.hozzaad(new Ital("Bor", 1200, Reszleg.SZESZES, "Tokaji", 5.2345));
-    System.out.println(bolt.toString());
-    System.out.println(bolt.toString(Reszleg.TEJ));
-    System.out.println(bolt.bejaras());
-    System.out.println(bolt.osszegar());
+      bolt.hozzaad(new Termek("Tej", 300, Reszleg.TEJ));
+      bolt.hozzaad(new Etel("Oreo", 520, Reszleg.KEKSZ, Iz.EDES));
+      bolt.hozzaad(sajt);
+      bolt.hozzaad(new TartosCikk("Monitor", 50000, Reszleg.ELEKTRONIKAI, "Samsung", 2));
+      bolt.hozzaad(new Ital("Bor", 1200, Reszleg.SZESZES, "Tokaji", 5.2345));
+      System.out.println(bolt.toString());
+      System.out.println(bolt.toString(Reszleg.TEJ));
+      System.out.println(bolt.bejaras());
+      System.out.println(bolt.osszegar());
 
-    List talalat = bolt.altalanosKereses("Tej");
-    for (int i = 0; i < talalat.size(); ++i) {
-      System.out.println(talalat.get(i).toString());
+      bolt.mentes();
+
+      List talalat = bolt.altalanosKereses("Tokaj");
+      for (int i = 0; i < talalat.size(); ++i) {
+        System.out.println(talalat.get(i).toString());
+      }
+
+      List talalat2 = bolt.alkoholosTermekek();
+      for (int i = 0; i < talalat2.size(); ++i) {
+        System.out.println(talalat2.get(i).toString());
+      }
+
+      bolt.eltavolit("Tej");
+      bolt.eltavolit(sajt);
+      System.out.println(bolt.osszegar());
+
+      sajt.setAr(0);
     }
-
-    bolt.eltavolit("Tej");
-    bolt.eltavolit(sajt);
-    System.out.println(bolt.osszegar());
-
+    catch (Exception e) {
+      e.printStackTrace();
+    }
   }
 }
